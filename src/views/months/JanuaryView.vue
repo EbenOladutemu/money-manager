7c1c10d0c909233d3aa6ef83e47dd40b1424b859
Ebenezer Oladutemu <ebenezer@plentify.co.za>
2024-03-03 21:39:03 +0100

    Some test stuff

--- src/views/months/JanuaryView.vue
+++ src/views/months/JanuaryView.vue
@@ -1,14 +1,56 @@
 <template>
   <div class="home">
     <ListComponent :msg="doughnut" />
+    <span
+      v-for="day in routine.days"
+      :key="day.name"
+      @click="day.selected = !day.selected"
+    >
+      {{ day.name }}
+      <span v-show="day.selected">Their own</span>
+    </span>
+  </div>
+  <div
+    style="margin-top: 3rem"
+    v-for="prop in properties"
+    :key="prop.id"
+    @click="chooseProp(prop)"
+  >
+    {{ prop.label }}
+    <p v-show="prop.isExpanded">LSA Zone</p>
+    <img src="@/assets/icons/delete.svg" :class="{ expand: prop.isExpanded }" />
   </div>
 </template>
 
 <script lang="ts" setup>
 import ListComponent from '@/components/ListComponent.vue'
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
 
 const doughnut = ref('Elereke Yum Yum')
+
+const activeProp = ref({})
+
+const chooseProp = (prop: any) => {
+  activeProp.value = prop
+  prop.isExpanded = !prop.isExpanded
+
+  // const not = properties.value.filter((prop) => prop.isExpanded == true)
+  // console.log(not)
+  // prop.label = Math.random()
+}
+
+const properties = ref([
+  {
+    id: 1,
+    label: 'Doughnut The Yum',
+    isExpanded: false,
+  },
+  {
+    id: 2,
+    label: 'Fluffy Yummy',
+    isExpanded: false,
+  },
+])
 
 const routine = ref({
   days: [
@@ -50,4 +92,22 @@ days.value.forEach((day) => {
     selectedDay.name == day ? (selectedDay.selected = true) : false
   })
 })
+
+onMounted(() => {
+  console.log(routine.value)
+})
 </script>
+
+<style>
+span {
+  margin-right: 1rem;
+}
+
+img {
+  transition: transform 0.5s;
+}
+
+.expand {
+  transform: rotate(45deg);
+}
+</style>
